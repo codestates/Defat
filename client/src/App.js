49387b994e6d906e44d89840c84ea446842f465a
Sun970324 <ysw5202222@gmail.com>
2022-05-11 21:05:33 +0900

    HeaderCSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,25 @@
-import React from "react";
-import Header from "./components/Header";
-import MainPage from "./pages/MainPage";
-import Mealkit from "./pages/Mealkit";
-import TodayMeal from "./pages/TodayMeal";
-import MyPage from "./pages/MyPage";
-import {
-  BrowserRouter as Router,
-  Route,Routes
-} from 'react-router-dom';
-import Footer from "./components/Footer";
-
-
-
+import React from 'react';
+import Header from './components/Header';
+import MainPage from './pages/MainPage';
+import Mealkit from './pages/Mealkit';
+import TodayMeal from './pages/TodayMeal';
+import MyPage from './pages/MyPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-    
-      <Header/>
+      <Header />
       <Routes>
-        <Route exact path ='/' element={<MainPage/>}/>
-          
-        
-        <Route path='/Mealkit'element={<Mealkit/>}/>
-          
-        
-        <Route path='/TodayMeal'element={<TodayMeal/>}/>
-          
-        
-        <Route path='/MyPage'element={<MyPage/>}/>
+        <Route exact path="/" element={<MainPage />} />
+        <Route path="/Mealkit" element={<Mealkit />} />
+        <Route path="/TodayMeal" element={<TodayMeal />} />
+        <Route path="/MyPage" element={<MyPage />} />
       </Routes>
-
-    <Footer/>
+      <Footer />
     </Router>
-    
-  )
+  );
 }
 
 export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,53 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-
-function Header(){
-    return (
-      <div>
-        <div id='login-header'>
-          <Link to ='/'><img src='img/logo2.png'/></Link>
-          <span>로그인</span>
-        </div>
-        <div id='menu-header'>
-          <Link to='/Mealkit'> <span>MealKit</span></Link>
-          <Link to='/TodayMeal'> <span>오늘의 메뉴</span></Link>
-          <Link to='/MyPage'>  <span>마이 페이지</span></Link>
-        
-       
-        </div>
-      </div>
-     
-    )
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+function Header() {
+  const Button = styled.button`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+`;
+  const Img = styled.img`
+    width: 16vh;
+    height: 7vh;
+  `
+  const Div = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  `
+  const ListDiv = styled.div`
+  background-color: gray;
+  flex: auto;
+  color: black;
+  text-align: center;
+  width: 33vw;
+  padding: 10px;
+  `
+  return (
+    <div>
+      <Div>
+        <Link to="/">
+          <Img src="img/logo2.png" />
+        </Link>
+        <Button>로그인</Button>
+      </Div>
+      <Div>
+        <Link to="/Mealkit">
+          <ListDiv>MealKit</ListDiv>
+        </Link>
+        <Link to="/TodayMeal">
+          <ListDiv>오늘의 메뉴</ListDiv>
+        </Link>
+        <Link to="/MyPage">
+          <ListDiv>마이 페이지</ListDiv>
+        </Link>
+      </Div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
