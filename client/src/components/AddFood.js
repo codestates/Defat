41917f13a11a82493b18c34e537dd6883c40679b
gamemakerr <geakura123@gmail.com>
2022-05-13 09:42:47 +0900

    5월 12일 수정본

--- client/src/components/AddFood.js
+++ client/src/components/AddFood.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
 import Modal from 'react-modal';
+import styled from 'styled-components';
+import Foodinfo from './FoodInfo';
+
+
+
 function AddFood() {
   const [open, setOpen] = useState(true)
   return (
     <Modal isOpen={open} onRequestClose={() => setOpen(false)}>
-      음식추가 모달
+      <input type='text'></input>
+      <button>검색</button>
+      <Foodinfo></Foodinfo>
     </Modal>
-  )
+  ) 
 }
 export default AddFood;
--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,10 +1,78 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
+import styled from 'styled-components';
+
+
+
 function LoginModal() {
   const [open, setOpen] = useState(true)
+
+  const RowDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  `
+  const ColumnDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+
+  const Logoimage = styled.img`
+    width: 16vh;
+    height: 16vh;
+  `
+  const OauthLogin = styled.div`
+    margin: 4px;
+    height:4vh;
+    width:33vw;
+    background-color: ${(props) => (props.primary === '1'? "yellow" :
+                                    props.primary === '2'? "green":'tomato')} ;
+    text-align: center;
+    flex: auto;
+    padding-top: 20px;
+  `
+  const Signupbutton= styled.button`
+      height:4vw;
+      width:8vw;
+      text-align: center;
+      padding:10px;
+      align-items: center;
+  `
+  
   return (
     <Modal isOpen={open} onRequestClose={() => setOpen(false)}>
-      로그인 모달
+      
+     <ColumnDiv>
+      <Logoimage src='img/logo.png'></Logoimage>
+      <RowDiv>
+      <ColumnDiv>
+        <div>아이디</div>
+        <div>비밀번호</div>
+      </ColumnDiv>
+        
+      <ColumnDiv>
+      <input type='text' defaultValue='아이디를 입력하세요'></input>
+      <input type='password' ></input>
+      </ColumnDiv>
+          
+
+      </RowDiv>
+      <RowDiv>
+        <input type='checkbox'></input>
+        <div>로그인 유지하기</div>
+        <a>아이디/비밀번호 찾기</a>
+      </RowDiv>
+      <OauthLogin primary='1'>카카오톡 로그인</OauthLogin>
+      <OauthLogin primary='2'>네이버 로그인</OauthLogin>
+      <OauthLogin >구글 로그인</OauthLogin>
+      <Signupbutton>회원가입</Signupbutton>
+     </ColumnDiv>
+     
+     
+     
     </Modal>
   )
 }
