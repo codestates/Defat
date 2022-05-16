964f818ad9aec08c17eec3a0bf9c5dd0a30196e7
gamemakerr <geakura123@gmail.com>
2022-05-17 01:47:07 +0900

    Css 수정본 거의 최종

--- client/src/pages/Mealkit.js
+++ client/src/pages/Mealkit.js
@@ -1,72 +1,100 @@
 import React from "react"
 import styled from "styled-components"
 import MealKitList from '../components/MealKitList'
-const Rowul = styled.ul`
+
+const CategoryContainer = styled.div`
+    height:100%;
+    width: 100%;
+    display:flex;
+    flex-direction: column;
+    background-color: white;
+    border: 2px;
+    border-color: gray;
+`
+const CategoryRow = styled.div`
+    border: 2px;
+    border-color: black;
+    width:100%;
+    height: 50px;
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
 `
-const CategoryName = styled.li`
- list-style: none;
- background-color: aqua;
+const Category = styled.div`
+    
+    width:15%;
+    height:100%;
+    background-color: aquamarine;
 `
-const TagDiv = styled.div`
-  height: 20px;
-  background-color: gray;
+
+const CategoryMenu =styled.div`
+    height: 100%;
+    background-color: beige;
+    width:100%;
 `
+
+const TagContainer =styled.div`
+    width:100%;
+    background-color: blueviolet;
+`
+const Tag=styled.div`
+
+`
+
 function Mealkit(){
   return (
     <div>
-      <form>
-        <input type='text'></input>
-        <button>검색</button>
-      </form>
-      <div className="category_container">
-        <div className="brand">
-          <Rowul>
-            <CategoryName>브랜드</CategoryName>
-            <li><a role='button'>푸드어셈블</a></li>
-            <li><a role='button'>ASHLEY</a></li>
-            <li><a role='button'>프레시지</a></li>
-            <li><a role='button'>마이셰프</a></li>
-            <li><a role='button'>아내의 셰프</a></li>
-          </Rowul>
-        </div>
-        <div className="kcal">
-          <Rowul>
-          <CategoryName>칼로리</CategoryName>
-            <li><a role='button'>200미만</a></li>
-            <li><a role='button'>200~300</a></li>
-            <li><a role='button'>300~400</a></li>
-            <li><a role='button'>400~500</a></li>
-            <li><a role='button'>500이상</a></li>
-          </Rowul>
-        </div>
-        <div className="price">
-          <Rowul>
-          <CategoryName>가격</CategoryName>
-            <li><a role='button'>2000원 미만</a></li>
-            <li><a role='button'>2000원~3000원</a></li>
-            <li><a role='button'>3000원~5000원</a></li>
-            <li><a role='button'>5000원~10000원</a></li>
-            <li><a role='button'>10000원 이상</a></li>
-          </Rowul>
+        <CategoryContainer>
+          <CategoryRow>
+          <input type='text'></input>
+          <button>검색</button>
+          </CategoryRow>
+        <CategoryRow>
+            <Category>브랜드</Category>
+            
+              <CategoryRow>
+                <CategoryMenu>잇메이트</CategoryMenu>
+                <CategoryMenu>맛있닭</CategoryMenu>
+                <CategoryMenu>마이비밀</CategoryMenu>
+                <CategoryMenu>허닭</CategoryMenu>
+              </CategoryRow>
+          
+        </CategoryRow>
+        <CategoryRow>
+            <Category>가격</Category>
+        
+              <CategoryRow>
+                <CategoryMenu>2천원~6천원</CategoryMenu>
+                <CategoryMenu>1만원~2만원</CategoryMenu>
+                <CategoryMenu>3만원~4만원</CategoryMenu>
+                <CategoryMenu>그 이상</CategoryMenu>
+              </CategoryRow>
+          
+        </CategoryRow>
+        <CategoryRow>
+            <Category>kcal</Category>
+            
+              <CategoryRow>
+              <CategoryMenu>100~200kcal</CategoryMenu>
+              <CategoryMenu>200~300kcal</CategoryMenu>
+              <CategoryMenu>300~400kcal</CategoryMenu>
+              <CategoryMenu>400~500kcal</CategoryMenu>
+              <CategoryMenu>500~600kcal</CategoryMenu>
+              </CategoryRow>
+            
+        </CategoryRow>
+        <TagContainer></TagContainer>
+        </CategoryContainer>
+      
+        <div>
+          <MealKitList />
+          <MealKitList />
+          <MealKitList />
+          <MealKitList />
         </div>
       </div>
-      <br></br>
-      <TagDiv>
-        태그 위치
-      </TagDiv>
-      <br></br>
-      <div>
-        <MealKitList />
-        <MealKitList />
-        <MealKitList />
-        <MealKitList />
-      </div>
-    </div>
-  )
+     )
 }
 
 
