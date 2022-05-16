482a94a4c3b7c545a258fc8e8cd9ca9a0d3334eb
gamemakerr <geakura123@gmail.com>
2022-05-16 16:52:04 +0900

    메인페이지1차수정본

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -43,7 +43,30 @@ function LoginModal() {
   `
   
   return (
-    <Modal isOpen={open} onRequestClose={() => setOpen(false)}>
+    <Modal isOpen={open} onRequestClose={() => setOpen(false)} style={{
+      overlay: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(255, 255, 255, 0.75)'
+      },
+      content: {
+        position: 'absolute',
+        top: '40px',
+        left: '40px', 
+        right: '40px',
+        bottom: '40px',
+        border: '1px solid #ccc',
+        background: '#fff',
+        overflow: 'auto',
+        WebkitOverflowScrolling: 'touch',
+        borderRadius: '4px',
+        outline: 'none',
+        padding: '20px'
+      }
+    }} >
       
      <ColumnDiv>
       <Logoimage src='img/logo.png'></Logoimage>
