f9d99437f09bcf938748e881814cc19b4d93fda8
Sun970324 <ysw5202222@gmail.com>
2022-05-16 22:32:14 +0900

    [feat,CSS]AddFood,TodayMeal

--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-function FoodList() {
+function FoodList({setPickFood}) {
   const list = ['닭가슴살','아메리카노','삼겹살']
-
   return (
     <div>
       {list.map((el) => {
-        return <div key={el}>{el}</div>
+        return <div onClick={() => setPickFood(el)} key={el}>{el}</div>
       })}
     </div>
   )
