13d5e5dcedeabf664e46f1daff780f860dea6c16
YOONSHIN PARK <pus1789@gmail.com>
2022-05-17 20:35:57 +0900

    거의 수정

--- server/controllers/kits/information.js
+++ server/controllers/kits/information.js
@@ -1,5 +1,5 @@
 const { kit } = require('../../models');
-const { Op } = require('sequelize')
+const { Op } = require('sequelize');
 
 module.exports = {
   
@@ -10,19 +10,18 @@ get : async (req,res) => {
   const kitInfo = await kit.findAll({  
     where : {
       [Op.and] : [
-        { brand : { [Op.or]: [brand], [Op.eq]: null }},
-        { kind_of_food : { [Op.or]: [kind_of_food], [Op.eq]: null }},
-        { priceNum : { [Op.or]: [priceNum], [Op.eq]: null }},
-        { kcalNum : { [Op.or]: [kcalNum], [Op.eq]: null }}
+        brand ? { brand :  brand } : null,
+        kind_of_food ? { kind_of_food : kind_of_food }: null,
+        priceNum ? { priceNum :  priceNum } : null,
+        kcalNum ? { kcalNum :  kcalNum } : null
       ]
     }
   })
-
-  if (!kitInfo) {
-    res.status(404).send({message : '해당 자료 없음'})
-  } else {
-    res.status(200).send({data : kitInfo , message : '검색 성공'})
-  }
-
+  
+    if (!kitInfo) {
+      res.status(404).send({message : '해당 자료 없음'})
+    } else {
+      res.status(200).send({data : kitInfo , message : '검색 성공'})
+    }
   }
 }
