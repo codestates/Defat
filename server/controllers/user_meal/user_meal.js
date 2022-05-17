ddf5113bd913a7ff28209e000bb27db15dbe558f
YOONSHIN PARK <pus1789@gmail.com>
2022-05-17 22:38:49 +0900

    머지

--- server/controllers/user_meal/user_meal.js
+++ server/controllers/user_meal/user_meal.js
@@ -38,13 +38,8 @@ module.exports = {
       res.status(404).send({message : '해당 자료 없음'})
     } else {
       res.status(200).send({data : todaymenuInfo, message : '정보 전달 완료'})
-    }
-
-    
-    
-
-  },
-
+  }
+},
   post : async (req, res) => {
     // user_id 
     // req.body kit_id
