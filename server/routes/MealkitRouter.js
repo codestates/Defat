c08161c5ac3b738ea7ec48e0e2b5d01077b3b7f0
Sangkan-K <gimsanghan53@gmail.com>
2022-05-18 14:23:45 +0900

    fin?!

--- server/routes/MealkitRouter.js
+++ server/routes/MealkitRouter.js
@@ -4,6 +4,7 @@ const kits = require('../controllers/kits');
 router.get('/', kits.detail.get)
 router.get('/information', kits.information.get)
 router.get('/find/:searchText', kits.find.get)
+router.get('/ingredient', kits.ingredient.get)
 
 
 module.exports = router;
